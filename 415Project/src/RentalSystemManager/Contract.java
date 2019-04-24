6f28bf95470eea4e1cf7d5097997669ccdd96805
cansu <cansuyildirimc@gmail.com>
2019-04-24 11:07:56 +0300

    books

---
+++ 415Project/src/RentalSystemManager/Contract.java
@@ -0,0 +1,15 @@
+package RentalSystemManager;
+
+public abstract class Contract {
+    private User user;
+    private Publication publication;
+    private Date startDate;
+    private Date endDate;
+
+    public Contract(User user, Publication publication, Date startDate, Date endDate) {
+        this.user = user;
+        this.publication = publication;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+}
