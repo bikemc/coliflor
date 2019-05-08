ad01028c44fe75a8f17da5b4a7c95c97c1af985d
akturkkaan <kaan-akturk@windowslive.com>
2019-05-08 18:09:18 +0300

    hello

--- Coliflor/src/UI/ui_Place/PlaceSearchController.java
+++ Coliflor/src/UI/ui_Place/PlaceSearchController.java
@@ -1,4 +1,35 @@
 package UI.ui_Place;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 public class PlaceSearchController {
+    private static final int WIDTH = 900;
+    private static final int HEIGHT = 600;
+    private Scene ui_Place, ui_place_results;
+
+    public Scene initializeScene(String fxmlName) throws java.io.IOException
+    {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlName));
+        Parent root = (Parent)loader.load();
+        return new Scene(root, WIDTH,HEIGHT);
+    }
+
+    public void backMainPlacePage(ActionEvent event) throws Exception
+    {
+        ui_Place = initializeScene("ui_Place.fxml");
+        Stage primaryStage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        primaryStage.setScene(ui_Place);
+    }
+
+    public void openPlaceResultPage(ActionEvent event) throws Exception
+    {
+        ui_place_results = initializeScene("ui_place_results.fxml");
+        Stage primaryStage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        primaryStage.setScene(ui_place_results);
+    }
 }
