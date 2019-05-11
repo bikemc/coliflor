196d2fd1332a209ae5dc10b61c761cdf88015b47
akturkkaan <kaan-akturk@windowslive.com>
2019-05-11 11:14:57 +0300

    UI Update

--- Coliflor/src/UI/ui_Book/BookLoginController.java
+++ Coliflor/src/UI/ui_Book/BookLoginController.java
@@ -1,10 +1,16 @@
 package UI.ui_Book;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.event.ActionEvent;
 
@@ -33,5 +39,26 @@ public class BookLoginController {
         ui_MainPage = initializeScene("../ui_MainPage.fxml");
         Stage primaryStage = (Stage)((Node)event.getSource()).getScene().getWindow();
         primaryStage.setScene(ui_MainPage);
+    }
+
+    public void bookSignUp(ActionEvent event) throws Exception {
+      /*  new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                final Stage dialog = new Stage();
+                dialog.initModality(Modality.APPLICATION_MODAL);
+                //dialog.initOwner(primaryStage);
+                dialog.initModality(Modality.NONE);
+                VBox dialogVbox = new VBox(20);
+                dialogVbox.getChildren().add(new Text("This is a Dialog"));
+                Scene dialogScene = new Scene(dialogVbox, 300, 200);
+                dialog.setScene(dialogScene);
+                dialog.show();
+            }
+
+        };*/
+        Stage secondStage = new Stage();
+        secondStage.setScene(new Scene(new HBox(300, new Label("    Hello")), 300,200));
+        secondStage.show();
     }
 }
--- Coliflor/src/UI/ui_WorkLabor/workerController/WorkerLoginController.java
+++
@@ -1,4 +0,0 @@
-package UI.ui_WorkLabor.workerController;
-
-public class WorkerLoginController {
-}
